673e97a54d6920f336793faeb290eb3cbe270bb1
Inhagni <inhagni@gmail.com>
2025-01-26 03:13:10 +0100

    templates

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -2,31 +2,34 @@ from django.shortcuts import render
 
 # Create your views here.
 # mysite/myapp/views.py
-from django.http import HttpResponse, HttpRequest
+# from django.http import HttpResponse, HttpRequest
 
 
-# Поздравляю, это ваш первый контроллер, который может принять запрос и отдать ответ с текстом, больше ничего
-def main(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("Hey! It's your main view!!")
+# # Поздравляю, это ваш первый контроллер, который может принять запрос и отдать ответ с текстом, больше ничего
+# def main(request: HttpRequest) -> HttpResponse:
+#     return HttpResponse("Hey! It's your main view!!")
 
 
-def another(request: HttpRequest) -> HttpResponse:
-    return HttpResponse("It's another page!!")
+# def another(request: HttpRequest) -> HttpResponse:
+#     return HttpResponse("It's another page!!")
 
 
-def main_article(request: HttpRequest) -> HttpResponse:
-    return HttpResponse('There will be a list with articles')
+# def main_article(request: HttpRequest) -> HttpResponse:
+#     return HttpResponse('There will be a list with articles')
 
 
-def uniq_article(request: HttpRequest) -> HttpResponse:
-    return HttpResponse('This is a unique answer for a unique value')
+# def uniq_article(request: HttpRequest) -> HttpResponse:
+#     return HttpResponse('This is a unique answer for a unique value')
 
 
-def article(request: HttpRequest, article_id: int, name: str = '') -> HttpResponse:
-    return HttpResponse(
-        "This is an article #{}. {}".format(article_id, "Name of this article is {}".format(
-            name) if name else "This is unnamed article"))
+# def article(request: HttpRequest, article_id: int, name: str = '') -> HttpResponse:
+#     return HttpResponse(
+#         "This is an article #{}. {}".format(article_id, "Name of this article is {}".format(
+#             name) if name else "This is unnamed article"))
 
 
-def regex(request: HttpRequest, text: str) -> HttpResponse:
-    return HttpResponse(f"It's regexp with text: {text}")
+# def regex(request: HttpRequest, text: str) -> HttpResponse:
+#     return HttpResponse(f"It's regexp with text: {text}")
+
+def index(request):
+    return render(request, 'index.html')
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,14 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
-from myapp.views import main, another, regex
+from django.urls import path
+from myapp.views import index
+# from myapp.views import main, another, regex
 
 
 urlpatterns = [
-    path('', main),
-    path('some_url/', another),
-    path('article/', include('myapp.urls')),
-    re_path(r'^(?P<text>\d{3}[a-zA-Z]{2,}$)', regex),
-    path('admin/', admin.site.urls),
+    path('', index, name='index'),
+    # path('some_url/', another),
+    # path('article/', include('myapp.urls')),
+    # re_path(r'^(?P<text>\d{3}[a-zA-Z]{2,}$)', regex),
+    # path('admin/', admin.site.urls),
 ]
+
