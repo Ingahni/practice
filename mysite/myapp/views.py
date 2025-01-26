528271d515a0f6f08e0813ee8fa27221f1e7c97f
Inhagni <inhagni@gmail.com>
2025-01-26 02:19:52 +0100

    re-path

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -9,8 +9,10 @@ from django.http import HttpResponse, HttpRequest
 def main(request: HttpRequest) -> HttpResponse:
     return HttpResponse("Hey! It's your main view!!")
 
+
 def another(request: HttpRequest) -> HttpResponse:
     return HttpResponse("It's another page!!")
+
 
 def main_article(request: HttpRequest) -> HttpResponse:
     return HttpResponse('There will be a list with articles')
@@ -20,5 +22,11 @@ def uniq_article(request: HttpRequest) -> HttpResponse:
     return HttpResponse('This is a unique answer for a unique value')
 
 
-def article(request: HttpRequest, article_id: int) -> HttpResponse:
-    return HttpResponse(f"This is article #{article_id}.")
+def article(request: HttpRequest, article_id: int, name: str = '') -> HttpResponse:
+    return HttpResponse(
+        "This is an article #{}. {}".format(article_id, "Name of this article is {}".format(
+            name) if name else "This is unnamed article"))
+
+
+def regex(request: HttpRequest, text: str) -> HttpResponse:
+    return HttpResponse(f"It's regexp with text: {text}")
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,13 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from myapp.views import main, another, main_article, uniq_article, article
+from django.urls import path, include, re_path
+from myapp.views import main, another, regex
 
 
 urlpatterns = [
     path('', main),
     path('some_url/', another),
     path('article/', include('myapp.urls')),
+    re_path(r'^(?P<text>\d{3}[a-zA-Z]{2,}$)', regex),
     path('admin/', admin.site.urls),
 ]
